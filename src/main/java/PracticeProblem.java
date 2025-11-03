aded06d099d2fab2a7ce1e5e3f0f538d71e22f3c
rajeeve-ravi-ics4u1-01-25 <rajeeve.ravi@student.tdsb.on.ca>
2025-11-03 13:30:32 -0500

    Stu submit V1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,11 +1,46 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-
-	public static int min(int num1, int num2, int num3) {
-		return num1;
-	}
-
+    public static void main(String args[]) {
+        // You can add code here to test your methods if needed
+    }
+    // Method: Pluralize a word
+    public static String pluralize(String word) {
+        String lowerWord = word.toLowerCase();
+        if (lowerWord.endsWith("ife")) {
+            return "ives";
+        } else if (lowerWord.endsWith("ey")) {
+            return "eys";
+        } else if (lowerWord.endsWith("y")) {
+            return "ies";
+        } else {
+            return "s";
+        }
+    }
+    // Method: Find minimum of three integers
+    public static int min(int num1, int num2, int num3) {
+        int smallest = num1;
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+        return smallest;
+    }
+    // Method: Check if year is a leap year
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
 }
